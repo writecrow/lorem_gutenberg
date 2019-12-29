c98358d984b2a90f11f17d6c6bef3133bb3e6e92
markfullmer <mfullmer@gmail.com>
2019-12-29 11:37:52 -0600

    Add cors access controls to api

--- api/index.php
+++ api/index.php
@@ -21,5 +21,9 @@ if (isset($_GET['author']) && in_array($_GET['author'], array_keys($authors))) {
   $options['author'] = $_GET['author'];
 }
 $text = LoremGutenberg::generate($options);
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
 header('Content-Type: application/json');
 echo json_encode(['data' => $text]);
